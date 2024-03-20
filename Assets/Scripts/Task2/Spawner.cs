b7045c442fca0995f41126f23e9e745f6a2dcb23
Soham Ovhal <ovhalsoham@gmail.com>
2024-03-20 18:31:05 +0530

    Plugin_Spawner_LineDrawer

---
+++ Assets/Scripts/Task2/Spawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject circlePrefab;
+    public int minCircles = 5;
+    public int maxCircles = 10;
+    public float spawnArea = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCircles();
+    }
+
+    // Update is called once per frame
+    void SpawnCircles()
+    {
+        int circlesToSpawn = Random.Range(minCircles, maxCircles + 1);
+        for (int i = 0; i < circlesToSpawn; i++)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnArea, spawnArea), Random.Range(-spawnArea, spawnArea), 0);
+            Instantiate(circlePrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    public void Restart()
+    {
+        
+    }
+}
